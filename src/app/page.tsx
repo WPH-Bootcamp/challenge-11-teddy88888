1950a95c5ec6b61c4abe073d6977b0293585568a
teddy8888 <teddy12pd@gmail.com>
2026-02-12 18:19:54 +0700

    challenge 11 teddy-044

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { MusicPlayer } from "@/components/MusicPlayer";
+import MusicPlayer from "@/components/MusicPlayer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 p-16">
+    <main className="flex min-h-screen items-center justify-center bg-[#050505]">
       <MusicPlayer />
-    </div>
+    </main>
   );
 }
